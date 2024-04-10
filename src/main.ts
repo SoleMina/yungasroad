972db7c8e6856d1dd2197fd1be7cabebd6a34dd9
Karina Prado <karina.pradogutierrez@gmail.com>
2024-04-10 15:05:46 -0500

    feat: add vuetify config

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,23 @@ import store from "./store";
 import axios from "axios";
 import VueAxios from "vue-axios";
 
+// Vuetify
+import "vuetify/styles";
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+
+const vuetify = createVuetify({
+  components,
+  directives,
+});
 const app = createApp(App);
 
 // Use
 app.use(router);
 app.use(store);
 app.use(VueAxios, axios);
+app.use(vuetify);
 app.mount("#app");
 
 //Api UrlBase
